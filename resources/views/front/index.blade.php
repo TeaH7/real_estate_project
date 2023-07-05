19f6ac3296def13ee0157ef0e381efa80cfc4583
Iraklis-S <122298794+Iraklis-S@users.noreply.github.com>
2023-07-05 14:03:44 +0200

    day1

--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -18,4 +18,31 @@
 
     </div>
     <!-- Hero Section End -->
+
+
+    {{-- Section --}}
+    <section class="presentation-2">
+        
+        <div class="half">
+            <div class="div1"></div>
+            <h2>You're in good hands</h2>
+            <div class="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro, nisi libero nesciunt nam voluptate eos fugiat ipsa voluptas, voluptatibus, enim assumenda accusamus voluptatum similique beatae nemo! Accusamus eaque eligendi in debitis facilis beatae ipsam saepe quia doloribus, eius quam similique quibusdam fuga laborum sint qui corrupti? Sed similique, dolore laudantium voluptates deserunt asperiores dignissimos sit, consequatur minus, veniam earum? Soluta quas illum iste, laudantium nobis ad, aspernatur officia consequatur illo molestiae quae aliquid ab maiores? Ad, accusantium unde doloremque inventore quas eum, consequatur architecto non voluptas porro 
+                officiis dolores ipsum nobis quo, vel nam? Labore earum excepturi mollitia eaque cumque.
+            </div>
+            <div class="button-style half-btn">
+            <a href="#">TO PROPERTIES<i class="fa-solid fa-arrow-right-long"></i></a>
+              </div>
+        </div>
+        <div class="second-half">
+            <img src="{{asset('assets/front/images/realtor.jpg')}}" alt="">
+        </div>
+    </section>
+    {{--End- Section --}}
+    {{-- Above Footer Section --}}
+    <section class="above-footer button-style">
+        <div>FIND YOUR DREAM <span>HOME</span></div>
+        <a href="#">TO PROPERTIES<i class="fa-solid fa-arrow-right-long"></i></a>
+
+    </section>
+    {{-- Above-footer End --}}
 @endsection

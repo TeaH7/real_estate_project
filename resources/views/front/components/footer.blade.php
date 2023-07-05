6a6a0618aebc37a6ffb6e0ad2029699017a13530
Iraklis-S <122298794+Iraklis-S@users.noreply.github.com>
2023-07-05 12:16:34 +0200

    Footer done , above-footer in progress
    Footer done , above-footer in progress

---
+++ resources/views/front/components/footer.blade.php
@@ -0,0 +1,91 @@
+<footer class="text-center text-lg-start text-muted">
+
+
+    <!-- Section: Links  -->
+    <section class="pt-3">
+        <div class="container text-center text-md-start mt-4">
+            <!-- Grid row -->
+            <div class="row mt-1">
+                <!-- Grid column -->
+                <div class="col-md-12 col-lg-4 col-xl-3 mx-2 mb-4 text-white">
+                    <!-- Content -->
+
+                    <div class="logo-socials-container">
+                    <a href="#">
+                  <div class="img-contain">  
+                    <img class="mb-2" src="{{ asset('assets/front/images/wheat.svg') }}" alt="Logo"
+                       >
+                    <span>LOGOIPSUM</span>
+                    </div>
+                </a>
+                    <div class="mt-3 socials">
+                        <a href="#">   <i class="fa-brands fa-square-facebook"></i></a>  <a href="#"><i class="fa-brands fa-instagram"></i></a>  <a href="#"><i class="fa-solid fa-envelope"></i></a>
+                    </div>
+                </div>
+                </div>
+                <!-- Grid column -->
+
+                <!-- Grid column -->
+                <div class="col-md-4 col-lg-2 col-xl-2 mx-auto mb-4 text-white click-links">
+                    <!-- Links -->
+                    <h6 class="text-uppercase fw-bold mb-4">
+                        Kompania
+                    </h6>
+                    <p>
+                     <a     href="#" class="text-reset">Rreth Nesh</a> 
+                    </p>
+                    <p>
+                        <a href="#" class="text-reset">Kontakt</a>
+                    </p>
+
+                </div>
+                <!-- Grid column -->
+
+                <!-- Grid column -->
+                <div class="col-md-4 col-lg-2 col-xl-2 mx-auto mb-4 text-white click-links">
+                    <!-- Links -->
+                    <h6 class="text-uppercase fw-bold mb-4">
+                        Prona
+                    </h6>
+                  
+                    <p>
+                        <a href="#" class="text-reset">Me Qera</a>
+                    </p>
+                    <p>
+                        <a href="#" class="text-reset">Ne Shitje </a>
+                    </p>
+                    <p>
+                        <a href="#" class="text-reset">Të Gjitha</a>
+                    </p>
+                </div>
+                <!-- Grid column -->
+
+                <!-- Grid column -->
+                <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4 text-white">
+                    <!-- Links -->
+                    <h6 class="text-uppercase fw-bold mb-4">Na Kontaktoni</h6>
+                    <p>
+                        <i class="bi bi-geo-alt me-1"></i>
+                        Rruga Don Bosko, Tiranë, Shqipëri
+                    </p>
+                    <p>
+                        <i class="bi bi-envelope me-1"></i>
+                        property@real-estate.com
+                    </p>
+                    <p><i class="bi bi-phone me-1"></i> +355 68 XX 58 XXX</p>
+
+                </div>
+                <!-- Grid column -->
+            </div>
+            <!-- Grid row -->
+        </div>
+    </section>
+    <!-- Section: Links  -->
+
+    <!-- Copyright -->
+    <div class="text-center p-4 text-white" style="background-color: var(--secondary-color);">
+        © <span id="year"></span> Copyright:
+        <a class="text-reset fw-bold" href="/">LOGOIPSUM</a>
+    </div>
+    <!-- Copyright -->
+</footer>
---
+++ resources/views/front/components/section-last.blade.php
@@ -0,0 +1,4 @@
+<section class="above-footer">
+<div>MAKE YOUR DREAMS A <span>REALITY</span></div>
+<button>WORK WITH US <i class="fa-sharp fa-light fa-arrow-right"></i></button>
+</section>

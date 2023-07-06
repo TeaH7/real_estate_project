d0ddc8fd7b6b5bd548e40ab40198bf38a31ae4b1
Tea Haxhiu <tea.haxhiu@gmail.com>
2023-07-06 16:01:29 +0200

    fix footer

--- resources/views/front/components/footer.blade.php
+++ resources/views/front/components/footer.blade.php
@@ -14,7 +14,7 @@
                         <a href="#">
                             <div class="img-contain">
                                 <img class="mb-2" src="{{ asset('assets/front/images/wheat.svg') }}" alt="Logo">
-                                <span>TIG ESTATE</span>
+                                <span>LOGOIPSUM</span>
                             </div>
                         </a>
                         <div class="mt-3 socials">
@@ -23,69 +23,70 @@
                                     class="fa-solid fa-envelope"></i></a>
                         </div>
                     </div>
-                    <!-- Grid column -->
-
-                    <!-- Grid column -->
-                    <div class="col-md-4 col-lg-2 col-xl-2 mx-auto mb-4 text-white click-links">
-                        <!-- Links -->
-                        <h6 class="text-uppercase fw-bold mb-4">
-                            Kompania
-                        </h6>
-                        <p>
-                            <a href="#" class="text-reset">Rreth Nesh</a>
-                        </p>
-                        <p>
-                            <a href="#" class="text-reset">Kontakt</a>
-                        </p>
-
-                    </div>
-                    <!-- Grid column -->
-
-                    <!-- Grid column -->
-                    <div class="col-md-4 col-lg-2 col-xl-2 mx-auto mb-4 text-white click-links">
-                        <!-- Links -->
-                        <h6 class="text-uppercase fw-bold mb-4">
-                            Prona
-                        </h6>
-
-                        <p>
-                            <a href="#" class="text-reset">Me Qera</a>
-                        </p>
-                        <p>
-                            <a href="#" class="text-reset">Ne Shitje </a>
-                        </p>
-                        <p>
-                            <a href="#" class="text-reset">Të Gjitha</a>
-                        </p>
-                    </div>
-                    <!-- Grid column -->
-
-                    <!-- Grid column -->
-                    <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4 text-white">
-                        <!-- Links -->
-                        <h6 class="text-uppercase fw-bold mb-4">Na Kontaktoni</h6>
-                        <p>
-                            <i class="bi bi-geo-alt me-1"></i>
-                            Rruga Don Bosko, Tiranë, Shqipëri
-                        </p>
-                        <p>
-                            <i class="bi bi-envelope me-1"></i>
-                            property@real-estate.com
-                        </p>
-                        <p><i class="bi bi-phone me-1"></i> +355 68 XX 58 XXX</p>
-
-                    </div>
-                    <!-- Grid column -->
                 </div>
-                <!-- Grid row -->
+                <!-- Grid column -->
+
+                <!-- Grid column -->
+                <div class="col-md-4 col-lg-2 col-xl-2 mx-auto mb-4 text-white click-links">
+                    <!-- Links -->
+                    <h6 class="text-uppercase fw-bold mb-4">
+                        Kompania
+                    </h6>
+                    <p>
+                        <a href="#" class="text-reset">Rreth Nesh</a>
+                    </p>
+                    <p>
+                        <a href="#" class="text-reset">Kontakt</a>
+                    </p>
+
+                </div>
+                <!-- Grid column -->
+
+                <!-- Grid column -->
+                <div class="col-md-4 col-lg-2 col-xl-2 mx-auto mb-4 text-white click-links">
+                    <!-- Links -->
+                    <h6 class="text-uppercase fw-bold mb-4">
+                        Prona
+                    </h6>
+
+                    <p>
+                        <a href="#" class="text-reset">Me Qera</a>
+                    </p>
+                    <p>
+                        <a href="#" class="text-reset">Ne Shitje </a>
+                    </p>
+                    <p>
+                        <a href="#" class="text-reset">Të Gjitha</a>
+                    </p>
+                </div>
+                <!-- Grid column -->
+
+                <!-- Grid column -->
+                <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4 text-white">
+                    <!-- Links -->
+                    <h6 class="text-uppercase fw-bold mb-4">Na Kontaktoni</h6>
+                    <p>
+                        <i class="bi bi-geo-alt me-1"></i>
+                        Rruga Don Bosko, Tiranë, Shqipëri
+                    </p>
+                    <p>
+                        <i class="bi bi-envelope me-1"></i>
+                        property@real-estate.com
+                    </p>
+                    <p><i class="bi bi-phone me-1"></i> +355 68 XX 58 XXX</p>
+
+                </div>
+                <!-- Grid column -->
             </div>
+            <!-- Grid row -->
+        </div>
     </section>
     <!-- Section: Links  -->
 
     <!-- Copyright -->
     <div class="text-center p-4 text-white" style="background-color: var(--secondary-color);">
         © <span id="year"></span> Copyright:
-        <a class="text-reset fw-bold" href="/">TIG ESTATE</a>
+        <a class="text-reset fw-bold" href="/">TIG Estate</a>
     </div>
     <!-- Copyright -->
 </footer>

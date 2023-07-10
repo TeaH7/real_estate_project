f0f57e8785a43c289bf9e8ff918636260950047a
Tea Haxhiu <tea.haxhiu@gmail.com>
2023-07-10 15:28:16 +0200

    CHanges

--- routes/web.php
+++ routes/web.php
@@ -39,32 +39,17 @@ Route::middleware(['auth'])->prefix('admin')->group(function () {
     })->name('admin');
 
     Route::get('/properties', [PropertyController::class, 'index'])->name('properties.index');
+    Route::get('/properties/create', [PropertyController::class, 'create'])->name('properties.create');
+    Route::post('/properties', [PropertyController::class, 'store'])->name('properties.store');
 
-    Route::get('/agents',[AgentController::class,'show'])->name('agents.index');
-    Route::get('/agents/create',[AgentController::class,'create'])->name('agents.create');
-    Route::post('/agents/create',[AgentController::class,'store'])->name('agents.store');
-    Route::get('/agents/edit/{id}',[AgentController::class,'update'])->name('agents.edit');
+    Route::get('/agents', [AgentController::class, 'show'])->name('agents.index');
+    Route::get('/agents/create', [AgentController::class, 'create'])->name('agents.create');
+    Route::post('/agents/create', [AgentController::class, 'store'])->name('agents.store');
+    Route::get('/agents/edit/{id}', [AgentController::class, 'update'])->name('agents.edit');
     // Route::put('/update-agent',[AgentController::class,'update'])->name('agents.update');
 });
 
 
 Auth::routes(['register' => false]);
-
-// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-
-
-
-Route::middleware(['auth'])->prefix('admin')->group(function () {
-    Route::get('/', function () {
-        return view('admin.adminIndex');
-    })->name('admin');
-
-    Route::get('/properties', [PropertyController::class, 'index'])->name('properties.index');
-    Route::get('/properties/create', [PropertyController::class, 'create'])->name('properties.create');
-    Route::post('/properties', [PropertyController::class, 'store'])->name('properties.store');
-});
-
-Auth::routes();
 
 // Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
